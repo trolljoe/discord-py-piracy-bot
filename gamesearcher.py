e3ad9dd8bbb413430dd7911eb86ca3706594e788
joetroll <161910543+trolljoe@users.noreply.github.com>
2024-05-30 20:56:06 -0300

    Update gamesearcher.py

--- gamesearcher.py
+++ gamesearcher.py
@@ -2,14 +2,15 @@ import requests
 
 class Search:
     def __init__(self):
-        self.base_url = "https://gamestatus.info/back/api/gameinfo/game/search_title/"
+        self.search_url = "https://gamestatus.info/back/api/gameinfo/game/search_title/"
+        self.popular_url = "https://gamestatus.info/back/api/gameinfo/game/referralsgame/"
 
     def search_game(self, title):
         # Prepare the payload with the title
         payload = {"title": title}
         
         # Make the POST request
-        response = requests.post(self.base_url, json=payload)
+        response = requests.post(self.search_url, json=payload)
         
         # Check if the request was successful
         if response.status_code == 200:
@@ -18,9 +19,18 @@ class Search:
             print(f"Failed to fetch game info. Status code: {response.status_code}")
             return None
 
+    def popular_games(self): # this is a really big output
+        response = requests.get(self.popular_url)
+
+        if response.status_code == 200:
+            return response.json()
+        else:
+            print(f"Failed to fetch popular games. Status code: {response.status_code}")
+            return None
+
 # Example usage
 if __name__ == "__main__":
-    search = Search()
-    game_info = search.search_game("gasasasas")
-    if game_info:
-        print(game_info)
+    game_search = Search()
+    popular_games_info = game_search.popular_games()
+    if popular_games_info:
+        print(popular_games_info)
